75f9457a2d505d30478332a8413d98a970b34705
Antonov Andrew <antonov.andrew.jr@gmail.com>
2021-07-02 16:40:28 +0500

    Рабочая стурктура

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./components/index/index');
+var usersRouter = require('./components/users/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'components'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -35,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('commonViews/error');
 });
 
 module.exports = app;
