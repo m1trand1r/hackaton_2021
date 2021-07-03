9d03a3d93bdd729f0df36d0f062ff9a40f89995c
Antonov Andrew <antonov.andrew.jr@gmail.com>
2021-07-03 18:48:04 +0500

    Поиск

--- components/search/searchController.js
+++ components/search/searchController.js
@@ -53,14 +53,45 @@ module.exports.postSearch = async (req, res, next) => {
                 let documentsNumbers = [];
                 if (req.body.filter && req.body.filter != 'default') {
                     documentsNumbers = (await searchModel.getDocumentNumber(req.body.text, req.body.filter)).flat();
-                    console.log(documentsNumbers);
                     result = files.filter(file => documentsNumbers.some(doc => file.split('/')[3].toLocaleLowerCase().includes(doc)));
                 } else {
                     text = req.body.text;
-                    result = files.filter(file => file.toLocaleLowerCase().includes(req.body.text.toLocaleLowerCase()));
+                    result = files.filter(file => {
+                        let lowerFile = file.toLocaleLowerCase();
+                        let lowerFileArr = lowerFile.split('/');
+                        let lowerText = req.body.text.toLocaleLowerCase();
+                        if (lowerFileArr[1].includes(lowerText) || lowerFileArr[2].includes(lowerText) || lowerFileArr[3].includes(lowerText)) {
+                            return true
+                        }
+                        return false;
+                    });
                 }
-                console.log(result);
-                res.json(result);
+                result = result.map(doc => {
+                    let docArr = doc.split('/');
+                    return {
+                        path: doc,
+                        fund: docArr[1],
+                        inventory: docArr[2],
+                        document: docArr[3],
+                        imageName: docArr[4]
+                    }
+                });
+                let resultMap = new Map();
+                for (let doc of result) {
+                    let key = `${doc.fund}/${doc.inventory}/${doc.document}`;
+                    if (!resultMap.has(key)) {
+                        resultMap.set(key, [{
+                            imageName: doc.imageName,
+                            path: doc.path
+                        }]);
+                    } else {
+                        resultMap.set(key, [{
+                            imageName: doc.imageName,
+                            path: doc.path
+                        }, ...resultMap.get(key)]);
+                    }
+                }
+                res.json(Object.fromEntries(resultMap.entries()));
             });
         } else {
             throw new Error('Body пуст.');
---
+++ public/javascripts/search.js
@@ -0,0 +1,79 @@
+function postFetchWrapper(url, body) {
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(body)
+        })
+        .then(response => response.json())
+        .then(result => {
+            resolve(result);
+        })
+        .catch(err => {
+            reject(err);
+        });
+    });
+}
+
+function renderResult(documents) {
+    let container = document
+    .getElementById('search-results');
+    for (let key in documents) {
+        let documentContainer = document.createElement('a');
+        documentContainer.classList.add('doc-link');
+        console.log(documents[key]);
+        documentContainer.href="#";
+        documentContainer.innerHTML = `<div class='document-name'>
+            ${key}
+        </div>
+        <div class='document-count'>
+            ${documents[key].length}
+        </div>
+        <div class='add-remove-btn' title='Добавить'>
+            
+        </div>`;
+        container.append(documentContainer);
+    }
+}
+
+function clearDocumentsContainer() {
+    document
+    .getElementById('search-results').innerHTML = '';
+}
+
+async function search() {
+    let searchInput =  document
+    .getElementById('search-input');
+    let filterSelect = document
+    .getElementById('category');
+
+    clearDocumentsContainer();
+
+    let text = searchInput.value.replace(/\s+/g, ' ').trim(); // Очистка текста для поиска от лиших пробелов
+    searchInput.value = text;
+
+    let filter = filterSelect.value;
+
+    postFetchWrapper('search', {
+        text: text,
+        filter: filter
+    })
+    .then(documents => {
+        renderResult(documents);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+function searchAddListener() {
+    document
+    .getElementById('search-button')
+    .addEventListener('click', search);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    searchAddListener();
+});
